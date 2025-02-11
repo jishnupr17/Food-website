6245b60b32ee7c5cd24747ce11be3c25cb493a45
jishnu pr <jishnupr812948@gmail.com>
2025-02-11 11:45:31 +0530

    Update Login.jsx

--- frondend/src/pages/Login.jsx
+++ frondend/src/pages/Login.jsx
@@ -39,7 +39,7 @@ const Signup = () => {
 
     try {
       const response = await fetch(
-        `${import.meta.env.VITE_REACT_APP_SERVER_DOMAIN}/login`,
+        `${import.meta.env.https://food-website-backend-cecq.onrender.com}/login`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
